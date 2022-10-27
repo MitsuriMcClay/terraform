ffed7d728d19a04a8626ca964026840d77d819fa
Mitsuri McClay <mcclay.mitsuri@gmail.com>
2022-10-27 20:48:24 +0900

    ansible provisioning

--- main.tf
+++ main.tf
@@ -18,15 +18,30 @@ resource "aws_internet_gateway" "IGW" {    # Creating Internet Gateway
 resource "aws_subnet" "mysubnet" {    # Creating Public Subnets
   vpc_id =  aws_vpc.main.id
    cidr_block = "10.0.0.0/24"        # CIDR block of public subnets
+  availability_zone = "ap-northeast-1a"
  }
 
- resource "aws_instance" "example" {
- ami = "ami-0f36dcfcc94112ea1"
- instance_type = "t2.micro"
- subnet_id = aws_subnet.mysubnet.id
- key_name= "key0921"
- associate_public_ip_address  = true
- vpc_security_group_ids = [aws_security_group.allow_ssh_http.id] 
+resource "aws_subnet" "mysubnet_2" {    # Creating Public Subnets
+  vpc_id =  aws_vpc.main.id
+   cidr_block = "10.0.1.0/24"        # CIDR block of public subnets
+  availability_zone = "ap-northeast-1c"
+ }
+
+resource "aws_instance" "example" {
+   count = 2
+   tags = {
+    Name = "test-instance-${count.index}"
+    }
+   ami = "ami-0f36dcfcc94112ea1"
+   instance_type = "t2.micro"
+   subnet_id = aws_subnet.mysubnet.id
+   key_name= "key0921"
+   associate_public_ip_address  = true
+   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id] 
+
+   provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user  -i '${self.public_ip},' --private-key ../Ansible_testproject/key0921.pem  ../Ansible_testproject/install_nginx.yml"
+  }
  }
 
 # Security Group
@@ -41,8 +56,8 @@ resource "aws_security_group" "allow_ssh_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = var.port
+    to_port = var.port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -51,9 +66,16 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
+#Load Balancer
 
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_ssh_http.id]
+  subnets            = [aws_subnet.mysubnet.id, aws_subnet.mysubnet_2.id]
+}
 
- 
 
 # Route Table
 
@@ -62,6 +84,34 @@ resource "aws_route_table" "example" {
 
   tags = {
     Name = "example"
+  }
+}
+
+#Target Group
+resource "aws_lb_target_group" "alb-example" {
+  name        = "tf-example-lb-alb-tg"
+  port        = var.port
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+}
+
+#Target Group Attachment
+resource "aws_lb_target_group_attachment" "alb-example_attach" {
+  count = 2
+  target_group_arn = aws_lb_target_group.alb-example.arn
+  target_id        = aws_instance.example[count.index].id
+  port             = var.port
+}
+
+#ALB Listner
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = var.port
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-example.arn
   }
 }
 
